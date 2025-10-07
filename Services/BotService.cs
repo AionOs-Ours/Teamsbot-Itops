5cd32092e6a08ddd5f5f0d696169626897462392
SChinmaya15 <81362092+SChinmaya15@users.noreply.github.com>
2025-10-07 15:51:36 +0530

    Updated the to give list

--- Services/BotService.cs
+++ Services/BotService.cs
@@ -152,9 +152,9 @@ namespace TeamsBot.Services
                     }
                     if (isFirst)
                     {
-                        var isInstallation = userText.ToLower().Contains("install ") || userText.ToLower().Contains("notepad++ ");
-                        var isList = await _geminiService.GetIsListGeminiResponseAsync(userText);
-                        if (isList && !isInstallation)
+                        var isInstallation = false;//userText.ToLower().Contains("install ") || userText.ToLower().Contains("notepad++ ");
+                        var isList = userText.ToLower().Contains("software ");//await _geminiService.GetIsListGeminiResponseAsync(userText);
+                        if (true)//(isList && !isInstallation)
                         {
 
                             foreach (var item in softwareSuites.Result.Children<JObject>().ToArray())
